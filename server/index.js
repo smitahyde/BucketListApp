82ee79eea40841e4e8bf86e5ed1b71a0c57dbee2
Smita Hyde <smitahyde@gmail.com>
2017-09-18 16:04:49 -0400

    update router .js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-var express = require ('express');
-var http = require ('http');
-var bodyParser = require ('body-parser');
-var app = express ();
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+var router = require('./router');
 
 app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
 var port = process.env.PORT || 3000;
 
