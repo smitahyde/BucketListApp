4d0c8bb76189f2b3ed918879eec29281ef77b385
Smita Hyde <smitahyde@gmail.com>
2017-09-21 16:13:40 -0400

    components part 1

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,13 @@
-import React, { Component} from 'react';
+import React from 'react';
+import { Component} from 'react';
+import NavBarHeader from './nav';
 
 	export default class App extends Component {
 		render() {
 			return (
-				<div>React Hello World</div>
+				<div>
+					<NavBarHeader/>
+				</div>
 			);
 		}
 	}
